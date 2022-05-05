92fee8b30d9f1e06f9a0808d467d83296bc9b609
Martijn de Vos <mdmartijndevos@gmail.com>
2022-05-05 14:35:54 +0200

    Removed exclude_peer argument

--- accdfl/core/sample_manager.py
+++ accdfl/core/sample_manager.py
@@ -1,5 +1,5 @@
 import hashlib
-from typing import List, Dict, Tuple
+from typing import List, Dict
 
 from accdfl.core.peer_manager import PeerManager
 
@@ -14,26 +14,24 @@ class SampleManager:
         self.peer_manager: PeerManager = peer_manager
         self.sample_size = sample_size
         self.num_aggregators = num_aggregators
-        self.sample_cache: Dict[Tuple[int, bytes], List[bytes]] = {}
+        self.sample_cache: Dict[int, List[bytes]] = {}
 
-    def get_ordered_sample_list(self, round: int, peers: List[bytes], exclude_peer: bytes = None) -> List[bytes]:
+    def get_ordered_sample_list(self, round: int, peers: List[bytes]) -> List[bytes]:
         peers = sorted(peers)
         hashes = []
         for peer_id in peers:
-            if peer_id == exclude_peer:
-                continue
             h = hashlib.md5(b"%s-%d" % (peer_id, round))
             hashes.append((peer_id, h.digest()))
         hashes = sorted(hashes, key=lambda t: t[1])
         return [t[0] for t in hashes]
 
-    def get_sample_for_round(self, round: int, exclude_peer: bytes = None) -> List[bytes]:
-        if (round, exclude_peer) in self.sample_cache:
-            return self.sample_cache[(round, exclude_peer)]
+    def get_sample_for_round(self, round: int) -> List[bytes]:
+        if round in self.sample_cache:
+            return self.sample_cache[round]
 
         peers = self.peer_manager.get_active_peers(round)
-        sample = self.get_ordered_sample_list(round, peers, exclude_peer=exclude_peer)[:self.sample_size]
-        self.sample_cache[(round, exclude_peer)] = sample
+        sample = self.get_ordered_sample_list(round, peers)[:self.sample_size]
+        self.sample_cache[round] = sample
 
         return sample
 
