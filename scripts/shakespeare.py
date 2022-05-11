2d0736e1e3e2577131cc2e00d810b94333bf886e
Martijn de Vos <mdmartijndevos@gmail.com>
2022-05-11 14:27:46 +0200

    Updated Shakespeare script

--- scripts/shakespeare.py
+++ scripts/shakespeare.py
@@ -1,14 +1,37 @@
+import os
+
 from accdfl.core.datasets.Shakespeare import Shakespeare, LSTM
 from accdfl.core.mappings import Linear
+from accdfl.core.model_trainer import ModelTrainer
 
-train_dir = "/Users/martijndevos/Documents/leaf/shakespeare/per_user_data/train"
-test_dir = "/Users/martijndevos/Documents/leaf/shakespeare/data/test"
+parameters = {
+        "batch_size": 20,
+        "target_participants": 100,
+        "dataset": "shakespeare",
+        "participants": ["a"],
+        "learning_rate": 0.8,
+        "momentum": 0,
+    }
+
+data_dir = "/Users/martijndevos/leaf/shakespeare"
+train_dir = os.path.join(data_dir, "per_user_data/train")
+test_dir = os.path.join(data_dir, "data/test")
 
 mapping = Linear(1, 100)
 s = Shakespeare(0, 0, mapping, train_dir=train_dir, test_dir=test_dir)
-print(s.get_trainset(batch_size=20, shuffle=True))
-print(s.get_testset())
+
+print("Datasets prepared")
 
 # Model
 model = LSTM()
 print(model)
+
+print("Initial evaluation")
+print(s.test(model))
+
+for round in range(3):
+        # Train
+        trainer = ModelTrainer(data_dir, parameters, 0)
+        trainer.train(model)
+        print("Training done")
+        print(s.test(model))
