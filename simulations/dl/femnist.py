24e9210c10e4e667fc8c91cf2bf41bb774da34b8
Martijn de Vos <mdmartijndevos@gmail.com>
2022-10-31 19:58:50 +0100

    Increased duration of FEMNIST experiments

--- simulations/dl/femnist.py
+++ simulations/dl/femnist.py
@@ -6,7 +6,7 @@ from simulations.dl.dl_simulation import DLSimulation
 
 if __name__ == "__main__":
     settings = SimulationSettings()
-    settings.duration = 3600 * 4 if "DURATION" not in os.environ else int(os.environ["DURATION"])
+    settings.duration = 3600 * 20 if "DURATION" not in os.environ else int(os.environ["DURATION"])
     settings.dataset = "femnist"
     settings.peers = 355
     settings.learning_rate = 0.004
