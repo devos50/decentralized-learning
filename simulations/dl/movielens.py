7e031ca466552a83287ab040f98997020d7dfc84
Martijn de Vos <mdmartijndevos@gmail.com>
2022-11-09 16:03:12 -0500

    Made acc logging interval flexible

--- simulations/dl/movielens.py
+++ simulations/dl/movielens.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     settings.num_aggregators = 1 if "NUM_AGGREGATORS" not in os.environ else int(os.environ["NUM_AGGREGATORS"])
     settings.fix_aggregator = bool(os.environ["FIX_AGGREGATOR"]) if "FIX_AGGREGATOR" in os.environ else False
     settings.batch_size = 20
-    settings.accuracy_logging_interval = 5
+    settings.accuracy_logging_interval = 5 if "ACC_LOG_INTERVAL" not in os.environ else int(os.environ["ACC_LOG_INTERVAL"])
     settings.latencies_file = "data/latencies.txt"
     settings.topology = "exp-one-peer"
     simulation = DLSimulation(settings)
