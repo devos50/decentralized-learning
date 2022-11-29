3d0c5827120f53118693f3b76d5644058402fdae
Martijn de Vos <mdmartijndevos@gmail.com>
2022-11-29 11:57:18 +0100

    Disabling checkpointing by default

--- simulations/dl/cifar10_iid.py
+++ simulations/dl/cifar10_iid.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     settings.learning_rate = 0.002
     settings.batch_size = 20
     settings.accuracy_logging_interval = 5 if "ACC_LOG_INTERVAL" not in os.environ else int(os.environ["ACC_LOG_INTERVAL"])
-    settings.checkpoint_interval = settings.accuracy_logging_interval if "CHECKPOINT_INTERVAL" not in os.environ else \
-        int(os.environ["CHECKPOINT_INTERVAL"])
+    settings.checkpoint_interval = None if "CHECKPOINT_INTERVAL" not in os.environ else int(os.environ["CHECKPOINT_INTERVAL"])
     settings.accuracy_device_name = "cpu" if "ACC_DEVICE_NAME" not in os.environ else os.environ["ACC_DEVICE_NAME"]
     settings.topology = "exp-one-peer"
     settings.latencies_file = "data/latencies.txt"
