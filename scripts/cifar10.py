227e5129e4539f0a9da4392b39adbb507e4e3758
Martijn de Vos <martijn.devos@epfl.ch>
2023-01-09 14:26:30 +0100

    Fixed CIFAR10 centralized learning

--- scripts/cifar10.py
+++ scripts/cifar10.py
@@ -1,6 +1,9 @@
+import asyncio
 import logging
 import os
 import time
+
+import torch
 
 from accdfl.core.mappings import Linear
 from accdfl.core.model_trainer import ModelTrainer
@@ -8,18 +11,18 @@ from accdfl.core.datasets.CIFAR10 import CIFAR10
 from accdfl.core.models import create_model
 from accdfl.core.session_settings import SessionSettings, LearningSettings
 
-NUM_ROUNDS = 3
+NUM_ROUNDS = 30
 
 logging.basicConfig(level=logging.DEBUG)
 
 learning_settings = LearningSettings(
     learning_rate=0.001,
-    momentum=0,
+    momentum=0.9,
     batch_size=200
 )
 
 settings = SessionSettings(
-    dataset="cifar10_niid",
+    dataset="cifar10",
     work_dir="",
     learning=learning_settings,
     participants=["a"],
@@ -29,7 +32,7 @@ settings = SessionSettings(
 
 data_dir = os.path.join(os.environ["HOME"], "dfl-data")
 
-mapping = Linear(1, 100)
+mapping = Linear(1, 1)
 s = CIFAR10(0, 0, mapping, train_dir=data_dir, test_dir=data_dir)
 
 print("Datasets prepared")
@@ -41,10 +44,19 @@ print(model)
 print("Initial evaluation")
 print(s.test(model))
 
-for round in range(NUM_ROUNDS):
-    start_time = time.time()
-    print("Starting training round %d" % (round + 1))
-    trainer = ModelTrainer(data_dir, settings, 0)
-    trainer.train(model)
-    print("Training round %d done - time: %f" % (round + 1, time.time() - start_time))
-    print(s.test(model))
+
+async def run():
+    for round in range(NUM_ROUNDS):
+        start_time = time.time()
+        print("Starting training round %d" % (round + 1))
+        trainer = ModelTrainer(data_dir, settings, 0)
+        await trainer.train(model)
+        print("Training round %d done - time: %f" % (round + 1, time.time() - start_time))
+        print(s.test(model))
+
+        # Save the model
+        torch.save(model.state_dict(), "cifar10.model")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
