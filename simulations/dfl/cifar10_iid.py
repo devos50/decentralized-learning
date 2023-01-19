04a48ec095da1d33cf9fba414065ba0b6639345a
Martijn de Vos <martijn.devos@epfl.ch>
2023-01-19 13:48:49 +0100

    Made accuracy logging interval flexible

--- simulations/dfl/cifar10_iid.py
+++ simulations/dfl/cifar10_iid.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     settings.num_aggregators = 1 if "NUM_AGGREGATORS" not in os.environ else int(os.environ["NUM_AGGREGATORS"])
     settings.fix_aggregator = bool(os.environ["FIX_AGGREGATOR"]) if "FIX_AGGREGATOR" in os.environ else False
     settings.batch_size = 20
-    settings.accuracy_logging_interval = 5
+    settings.accuracy_logging_interval = 5 if "ACC_LOG_INTERVAL" not in os.environ else int(os.environ["ACC_LOG_INTERVAL"])
     settings.train_device_name = "cpu" if "TRAIN_DEVICE_NAME" not in os.environ else os.environ["TRAIN_DEVICE_NAME"]
     settings.accuracy_device_name = "cpu" if "ACC_DEVICE_NAME" not in os.environ else os.environ["ACC_DEVICE_NAME"]
     settings.latencies_file = "data/latencies.txt"
