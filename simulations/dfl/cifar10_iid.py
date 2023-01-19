5eb9edff5b6af676094e068bc08d9f35ff89122a
Martijn de Vos <martijn.devos@epfl.ch>
2023-01-19 13:46:05 +0100

    Added support for different backends for DFL

--- simulations/dfl/cifar10_iid.py
+++ simulations/dfl/cifar10_iid.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
     settings.fix_aggregator = bool(os.environ["FIX_AGGREGATOR"]) if "FIX_AGGREGATOR" in os.environ else False
     settings.batch_size = 20
     settings.accuracy_logging_interval = 5
+    settings.train_device_name = "cpu" if "TRAIN_DEVICE_NAME" not in os.environ else os.environ["TRAIN_DEVICE_NAME"]
+    settings.accuracy_device_name = "cpu" if "ACC_DEVICE_NAME" not in os.environ else os.environ["ACC_DEVICE_NAME"]
     settings.latencies_file = "data/latencies.txt"
     simulation = DFLSimulation(settings)
     ensure_future(simulation.run())
