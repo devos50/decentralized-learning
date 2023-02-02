8b1a4f89067d485b17b9b36766a1040085e13c1a
Martijn de Vos <martijn.devos@epfl.ch>
2023-02-02 13:35:50 +0100

    Using args for standalone scripts

--- scripts/cifar10.py
+++ scripts/cifar10.py
@@ -1,19 +1,11 @@
 """
-Train n standalone CIFAR10 models for a specified number of rounds.
+Train n standalone Celeba models for a specified number of rounds.
 """
 import asyncio
 import logging
-import os
 
-from accdfl.core.session_settings import LearningSettings
-from scripts.run import run
-
-learning_settings = LearningSettings(
-    learning_rate=0.001 if "LEARNING_RATE" not in os.environ else float(os.environ["LEARNING_RATE"]),
-    momentum=0.9,
-    batch_size=200
-)
+from scripts.run import get_args, run
 
 logging.basicConfig(level=logging.INFO)
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run(learning_settings, "cifar10"))
+loop.run_until_complete(run(get_args(default_lr=0.001, default_momentum=0.9), "cifar10"))
