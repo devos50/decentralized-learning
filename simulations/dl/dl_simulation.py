2a62b639e6ee36c59b288d80de223b86283f6ff1
Martijn de Vos <martijn.devos@epfl.ch>
2023-02-08 14:35:49 +0100

    Added support for limited active participants in DL simulations

--- simulations/dl/dl_simulation.py
+++ simulations/dl/dl_simulation.py
@@ -1,6 +1,7 @@
 import os
 from asyncio import get_event_loop
 from binascii import hexlify
+from typing import List
 
 from accdfl.core.model_manager import ModelManager
 from accdfl.core.session_settings import LearningSettings, SessionSettings, DLSettings
@@ -16,6 +17,7 @@ class DLSimulation(LearningSimulation):
     def __init__(self, settings: SimulationSettings) -> None:
         super().__init__(settings)
         self.num_round_completed = 0
+        self.participants_ids: List[int] = []
 
     def get_ipv8_builder(self, peer_id: int) -> ConfigBuilder:
         builder = super().get_ipv8_builder(peer_id)
@@ -27,6 +29,17 @@ class DLSimulation(LearningSimulation):
 
     async def setup_simulation(self) -> None:
         await super().setup_simulation()
+
+        if self.settings.active_participants:
+            self.logger.info("Initial active participants: %s", self.settings.active_participants)
+            start_ind, end_ind = self.settings.active_participants.split("-")
+            start_ind, end_ind = int(start_ind), int(end_ind)
+            participants_pks = [hexlify(self.nodes[ind].overlays[0].my_peer.public_key.key_to_bin()).decode()
+                            for ind in range(start_ind, end_ind)]
+            self.participants_ids = list(range(start_ind, end_ind))
+        else:
+            participants_pks = [hexlify(node.overlays[0].my_peer.public_key.key_to_bin()).decode() for node in self.nodes]
+            self.participants_ids = list(range(len(self.nodes)))
 
         # Setup the training process
         learning_settings = LearningSettings(
@@ -41,7 +54,7 @@ class DLSimulation(LearningSimulation):
             work_dir=self.data_dir,
             dataset=self.settings.dataset,
             learning=learning_settings,
-            participants=[hexlify(node.overlays[0].my_peer.public_key.key_to_bin()).decode() for node in self.nodes],
+            participants=participants_pks,
             all_participants=[hexlify(node.overlays[0].my_peer.public_key.key_to_bin()).decode() for node in
                               self.nodes],
             target_participants=len(self.nodes),
@@ -68,20 +81,20 @@ class DLSimulation(LearningSimulation):
                 node.overlays[0].nodes = self.nodes
 
     def build_topology(self):
+        self.logger.info("Building a %s topology", self.session_settings.dl.topology)
         if self.session_settings.dl.topology == "ring":
             # Build a simple ring topology
-            print("Building a ring topology")
-            for ind, node in enumerate(self.nodes):
-                nb_node = self.nodes[(ind + 1) % len(self.nodes)]
-                node.overlays[0].neighbours = [nb_node.overlays[0].my_peer.public_key.key_to_bin()]
+            for ind in self.participants_ids:
+                nb_node = self.nodes[(ind + 1) % len(self.participants_ids)]
+                self.nodes[ind].overlays[0].neighbours = [nb_node.overlays[0].my_peer.public_key.key_to_bin()]
         elif self.session_settings.dl.topology == "exp-one-peer":
-            G = ExponentialTwoGraph(len(self.nodes))
-            for node_ind in range(len(self.nodes)):
+            G = ExponentialTwoGraph(len(self.participants_ids))
+            for node_ind in range(len(self.participants_ids)):
                 g = GetDynamicOnePeerSendRecvRanks(G, node_ind)
                 nb_ids = [next(g)[0][0] for _ in range(len(list(G.neighbors(node_ind))) - 1)]
                 for nb_ind in nb_ids:
-                    nb_pk = self.nodes[nb_ind].overlays[0].my_peer.public_key.key_to_bin()
-                    self.nodes[node_ind].overlays[0].neighbours.append(nb_pk)
+                    nb_pk = self.nodes[self.participants_ids[0] + nb_ind].overlays[0].my_peer.public_key.key_to_bin()
+                    self.nodes[self.participants_ids[0] + node_ind].overlays[0].neighbours.append(nb_pk)
         else:
             raise RuntimeError("Unknown DL topology %s" % self.session_settings.dl.topology)
 
@@ -89,7 +102,7 @@ class DLSimulation(LearningSimulation):
         self.num_round_completed += 1
         peer_pk = self.nodes[peer_ind].overlays[0].my_peer.public_key.key_to_bin()
         self.model_manager.incoming_trained_models[peer_pk] = self.nodes[peer_ind].overlays[0].model_manager.model
-        if self.num_round_completed < len(self.nodes):
+        if self.num_round_completed < len(self.session_settings.participants):
             return
 
         # Everyone completed their round - wrap up
@@ -98,12 +111,12 @@ class DLSimulation(LearningSimulation):
             tot_up += node.overlays[0].endpoint.bytes_up
             tot_down += node.overlays[0].endpoint.bytes_down
 
-        print("All peers completed round %d - bytes up: %d, bytes down: %d" % (round_nr, tot_up, tot_down))
+        self.logger.info("All peers completed round %d - bytes up: %d, bytes down: %d" % (round_nr, tot_up, tot_down))
         self.num_round_completed = 0
 
         # Compute model accuracy
         if round_nr % self.settings.accuracy_logging_interval == 0:
-            print("Will compute accuracy for round %d!" % round_nr)
+            self.logger.info("Will compute accuracy for round %d!" % round_nr)
             try:
                 if self.settings.dl_accuracy_method == DLAccuracyMethod.AGGREGATE_THEN_TEST:
                     avg_model = self.model_manager.aggregate_trained_models()
@@ -135,5 +148,5 @@ class DLSimulation(LearningSimulation):
 
         self.model_manager.reset_incoming_trained_models()
 
-        for node in self.nodes:
-            node.overlays[0].start_next_round()
+        for ind in self.participants_ids:
+            self.nodes[ind].overlays[0].start_next_round()
