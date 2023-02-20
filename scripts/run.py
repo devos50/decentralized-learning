bcd9202c38f23babcac882cf2bf50385f89a2167
Martijn de Vos <martijn.devos@epfl.ch>
2023-02-20 11:45:04 +0100

    Allowing training of different standalone model

--- scripts/run.py
+++ scripts/run.py
@@ -27,6 +27,7 @@ def get_args(default_lr: float, default_momentum: float = 0):
     parser.add_argument('--rounds', type=int, default=100)
     parser.add_argument('--acc-check-interval', type=int, default=1)
     parser.add_argument('--partitioner', type=str, default="iid")
+    parser.add_argument('--model', type=str, default=None)
     parser.add_argument('--train-method', type=str, default="local")
     parser.add_argument('--das-subprocess-jobs', type=int, default=1)
     parser.add_argument('--das-peers-per-subprocess', type=int, default=10)
@@ -50,6 +51,7 @@ async def run(args, dataset: str):
         participants=["a"],
         all_participants=["a"],
         target_participants=args.peers,
+        model=args.model,
     )
 
     data_path = os.path.join("data", "%s_n_%d" % (dataset, args.peers))
