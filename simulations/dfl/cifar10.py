c81520171c799d3f7379043dbaac824710399838
Martijn de Vos <mdmartijndevos@gmail.com>
2023-02-20 20:32:46 +0100

    Added parameters to DFL simulations

--- simulations/dfl/cifar10.py
+++ simulations/dfl/cifar10.py
@@ -15,11 +15,12 @@ if __name__ == "__main__":
     settings.peers = 100 if "NUM_PEERS" not in os.environ else int(os.environ["NUM_PEERS"])
     settings.active_participants = None if "ACTIVE_PARTICIPANTS" not in os.environ else os.environ["ACTIVE_PARTICIPANTS"]
     settings.momentum = 0.9 if "MOMENTUM" not in os.environ else float(os.environ["MOMENTUM"])
+    settings.weight_decay = 0.0 if "WEIGHT_DECAY" not in os.environ else float(os.environ["WEIGHT_DECAY"])
     settings.learning_rate = 0.002 if "LEARNING_RATE" not in os.environ else float(os.environ["LEARNING_RATE"])
     settings.sample_size = (settings.peers // 10) if "SAMPLE_SIZE" not in os.environ else int(os.environ["SAMPLE_SIZE"])
     settings.num_aggregators = 1 if "NUM_AGGREGATORS" not in os.environ else int(os.environ["NUM_AGGREGATORS"])
     settings.fix_aggregator = bool(os.environ["FIX_AGGREGATOR"]) if "FIX_AGGREGATOR" in os.environ else False
-    settings.batch_size = 20
+    settings.batch_size = 20 if "BATCH_SIZE" not in os.environ else int(os.environ["BATCH_SIZE"])
     settings.accuracy_logging_interval = 5 if "ACC_LOG_INTERVAL" not in os.environ else int(os.environ["ACC_LOG_INTERVAL"])
     settings.train_device_name = "cpu" if "TRAIN_DEVICE_NAME" not in os.environ else os.environ["TRAIN_DEVICE_NAME"]
     settings.accuracy_device_name = "cpu" if "ACC_DEVICE_NAME" not in os.environ else os.environ["ACC_DEVICE_NAME"]
