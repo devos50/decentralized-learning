67f8b242ed433dd1a6d2b869d7d7c9dd1f2fcc10
Martijn de Vos <mdmartijndevos@gmail.com>
2023-04-22 14:27:38 +0200

    Simplified DFL bypass

--- simulations/dfl/bypass_network_community.py
+++ simulations/dfl/bypass_network_community.py
@@ -1,12 +1,13 @@
 import asyncio
 import json
 import pickle
-from asyncio import sleep, ensure_future
+from asyncio import sleep, ensure_future, Future
 from typing import Optional, List, Tuple
 
 from accdfl.core.models import unserialize_model, serialize_model
 from accdfl.dfl.community import DFLCommunity
 from accdfl.util.eva.result import TransferResult
+from ipv8.types import Peer
 
 
 class DFLBypassNetworkCommunity(DFLCommunity):
@@ -17,40 +18,28 @@ class DFLBypassNetworkCommunity(DFLCommunity):
         self.bandwidth: Optional[float] = None
         self.transfers: List[Tuple[str, str, int, float, bool, str]] = []
 
-    async def eva_send_model(self, round, model, type, population_view, peer):
-        serialized_model = serialize_model(self.model_manager.model)
-        serialized_population_view = pickle.dumps(population_view)
-        transfer_size_kbits = (len(serialized_model) + len(serialized_population_view)) / 1024 * 8
+    def schedule_eva_send_model(self, peer: Peer, serialized_response: bytes, binary_data: bytes, start_time: float) -> Future:
+        # Schedule the transfer
+        future = ensure_future(self.bypass_send(peer, serialized_response, binary_data))
+        future.add_done_callback(lambda f: self.on_eva_send_done(f, peer, serialized_response, binary_data, start_time))
+        return future
+
+    async def bypass_send(self, peer: Peer, serialized_response: bytes, binary_data: bytes):
         found: bool = False
         for node in self.nodes:
             if node.overlays[0].my_peer == peer:
-                found = True
-
                 if not node.overlays[0].is_active:
                     break
+                found = True
 
-                # Simulate the time it takes for a transfer
                 if self.bandwidth:
+                    transfer_size_kbits = len(binary_data) / 1024 * 8
                     transfer_time = transfer_size_kbits / self.bandwidth
                     await sleep(transfer_time)
                     self.logger.info("Model transfer took %f s.", transfer_time)
 
-                success: bool = False
-                if node.overlays[0].is_active:
-                    success = True
-                    self.endpoint.bytes_up += len(serialized_model) + len(serialized_population_view)
-                    node.overlays[0].endpoint.bytes_down += len(serialized_model) + len(serialized_population_view)
-
-                    info = {"round": round, "type": type, "model_data_len": len(serialized_model)}
-                    transfer = TransferResult(self.my_peer, json.dumps(info).encode(),
-                                              serialized_model + serialized_population_view, 0)
-                    ensure_future(node.overlays[0].on_receive(transfer))
-
-                peer_pk = node.overlays[0].my_peer.public_key.key_to_bin()
-                cur_time = asyncio.get_event_loop().time()
-                type_short = type.split("_")[0]
-                self.transfers.append((self.peer_manager.get_my_short_id(), self.peer_manager.get_short_id(peer_pk), round, cur_time, success, type_short))
-
+                res = TransferResult(self.my_peer, serialized_response, binary_data, 0)
+                ensure_future(node.overlays[0].on_receive(res))
                 break
 
         if not found:
