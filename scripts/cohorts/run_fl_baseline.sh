d92a1079607190ce292d0ceb72929cc2740bbcc8
Martijn de Vos <martijn.devos@epfl.ch>
2023-06-12 14:45:32 +0200

    Increased rounds to 1000

--- scripts/cohorts/run_fl_baseline.sh
+++ scripts/cohorts/run_fl_baseline.sh
@@ -6,7 +6,7 @@ python3 -u simulations/dfl/cifar10.py \
 --activity-log-interval 60 \
 --accuracy-logging-interval 5 \
 --duration 0 \
---rounds 500 \
+--rounds 1000 \
 --fixed-training-time 30 \
 --bypass-model-transfers \
 --seed 24082 \
