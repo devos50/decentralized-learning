52677dd12f7be2e138085632800fbdb2a0c4fc72
Martijn de Vos <martijn.devos@epfl.ch>
2023-06-12 16:13:44 +0200

    Fixed timestamp

--- scripts/cohorts/distill.sh
+++ scripts/cohorts/distill.sh
@@ -2,7 +2,7 @@
 
 for timestamp in "$@"; do
   echo "Starting distillation process at time $timestamp"
-  python3 -u scripts/distill.py $PWD/data n_100_cifar10_s0_a1_sf1.000000_sd24082 cifar10 cifar100 --distill-timestamp 8400 > "distill_${timestamp}.log" 2>&1 &
+  python3 -u scripts/distill.py $PWD/data n_100_cifar10_s0_a1_sf1.000000_sd24082 cifar10 cifar100 --distill-timestamp $timestamp > "distill_${timestamp}.log" 2>&1 &
 done
 
 wait
