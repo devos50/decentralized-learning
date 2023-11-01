51fb03a73358e92354bcd93c75eb51b899987fc5
Martijn de Vos <martijn.devos@epfl.ch>
2023-11-01 10:51:29 +0100

    Fixed variable

--- scripts/cohorts/tune_lr_femnist.sh
+++ scripts/cohorts/tune_lr_femnist.sh
@@ -7,7 +7,7 @@ export COHORTS=4
 learning_rates=(0.0005 0.001 0.005 0.01 0.05 0.1)
 
 for lr in "${learning_rates[@]}"; do
-  python3 -u scripts/distill.py $PWD/data n_1000_femnist_dirichlet${ALPHA}00000_sd${SEED}_ct${COHORTS}_p${PARTICIPATION}_dfl femnist svhn --cohort-file cohorts/cohorts_n1000_c${COHORTS}_s${SEED}_a${ALPHA}.txt --cohort-participation-fraction $PARTICIPATION --public-data-dir /var/scratch/spandey/dfl-data/svhn --private-data-dir /var/scratch/spandey/leaf/femnist --learning-rate $lr --momentum 0.9 --partitioner dirichlet --alpha $ALPHA --seed $SEED --weighting-scheme label --check-teachers-accuracy > output_distill_femnist_c${COHORTS}_s${SEED}_a${ALPHA}_p${PARTICIPATION}_lr{$lr}.log &
+  python3 -u scripts/distill.py $PWD/data n_1000_femnist_dirichlet${ALPHA}00000_sd${SEED}_ct${COHORTS}_p${PARTICIPATION}_dfl femnist svhn --cohort-file cohorts/cohorts_n1000_c${COHORTS}_s${SEED}_a${ALPHA}.txt --cohort-participation-fraction $PARTICIPATION --public-data-dir /var/scratch/spandey/dfl-data/svhn --private-data-dir /var/scratch/spandey/leaf/femnist --learning-rate $lr --momentum 0.9 --partitioner dirichlet --alpha $ALPHA --seed $SEED --weighting-scheme label --check-teachers-accuracy > output_distill_femnist_c${COHORTS}_s${SEED}_a${ALPHA}_p${PARTICIPATION}_lr${lr}.log &
 done
 
 wait
