7969b8020883ccff3af0b69be491be2ebac30e72
Martijn de Vos <martijn.devos@epfl.ch>
2024-03-18 15:24:19 +0100

    Updated e2e scripts

--- scripts/cohorts/run_e2e_femnist.sh
+++ scripts/cohorts/run_e2e_femnist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ensure the script receives exactly two arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <number_of_cohorts> <seed> <alpha>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <number_of_cohorts> <seed> <alpha> <participation>"
     exit 1
 fi
 
@@ -10,10 +10,15 @@ fi
 COHORTS=$1
 SEED=$2
 ALPHA=$3
+PARTICIPATION=$4
+PATIENCE=100
+PEERS=3550
+LR=0.1
 
 # Create the cohort configuration
-python3 scripts/cohorts/create_cohort_file.py 355 $COHORTS --seed $SEED --partitioner dirichlet --alpha $ALPHA --output "data/cohorts/cohorts_n355_c${COHORTS}_s${SEED}_a${ALPHA}.txt"
+python3 -u scripts/cohorts/create_cohort_file.py $PEERS $COHORTS --seed $SEED --partitioner dirichlet --alpha $ALPHA --output "data/cohorts/cohorts_n${PEERS}_c${COHORTS}_s${SEED}_a${ALPHA}.txt" --dataset femnist --data-dir /mnt/nfs/devos/leaf/femnist
 
-python3 -u simulations/dfl/femnist.py --peers 355 --duration 0 --num-aggregators 1 --activity-log-interval 60 --bypass-model-transfers --seed $SEED --capability-trace data/client_device_capacity --accuracy-logging-interval 0 --partitioner dirichlet --alpha $ALPHA --fix-aggregator --cohort-file "cohorts/cohorts_n355_c${COHORTS}_s${SEED}_a${ALPHA}.txt" --compute-validation-loss-global-model --train-device-name "cuda:0" --accuracy-device-name "cuda:0" --log-level "ERROR" > output_femnist_c${COHORTS}_s${SEED}_a${ALPHA}.log 2>&1
+python3 -u simulations/dfl/femnist.py --dataset-base-path "/mnt/nfs/devos" --peers $PEERS --local-steps 5 --cohort-participation-fraction $PARTICIPATION --duration 0 --num-aggregators 1 --activity-log-interval 60 --bypass-model-transfers --seed $SEED --stop-criteria-patience $PATIENCE --capability-trace data/client_device_capacity --accuracy-logging-interval 0 --partitioner dirichlet --alpha $ALPHA --fix-aggregator --cohort-file "cohorts/cohorts_n${PEERS}_c${COHORTS}_s${SEED}_a${ALPHA}.txt" --compute-validation-loss-global-model --log-level "ERROR" --accuracy-logging-interval 10 --learning-rate $LR --checkpoint-interval 18000 --checkpoint-interval-is-in-sec > output_femnist_c${COHORTS}_s${SEED}_a${ALPHA}_p${PARTICIPATION}.log 2>&1
 
-python3 -u scripts/distill.py $PWD/data n_355_femnist_dirichlet${ALPHA}00000_sd${SEED}_ct${COHORTS}_dfl femnist svhn --cohort-file cohorts/cohorts_n355_c${COHORTS}_s${SEED}_a${ALPHA}.txt --public-data-dir /var/scratch/spandey/dfl-data --learning-rate 0.001 --momentum 0.9 --partitioner dirichlet --alpha $ALPHA --seed $SEED --weighting-scheme label --check-teachers-accuracy > output_distill_femnist_c${COHORTS}_s${SEED}_a${ALPHA}.log 2>&1
+python3 -u scripts/ensembles.py data n_${PEERS}_femnist_dirichlet${ALPHA}00000_sd${SEED}_ct${COHORTS}_p${PARTICIPATION}_dfl femnist --cohort cohorts/cohorts_n${PEERS}_c${COHORTS}_s${SEED}_a${ALPHA}.txt --partitioner dirichlet --alpha $ALPHA --seed $SEED --test-interval 5 --data-dir /mnt/nfs/devos/leaf/femnist > output_ensembles_femnist_n${PEERS}_c${COHORTS}.txt 2>&1 &
+#python3 -u scripts/distill.py $PWD/data n_1000_femnist_dirichlet${ALPHA}00000_sd${SEED}_ct${COHORTS}_p${PARTICIPATION}_dfl femnist svhn --cohort-file cohorts/cohorts_n1000_c${COHORTS}_s${SEED}_a${ALPHA}.txt --cohort-participation-fraction $PARTICIPATION --public-data-dir /var/scratch/spandey/dfl-data/svhn --private-data-dir /var/scratch/spandey/leaf/femnist --learning-rate 0.001 --momentum 0.9 --partitioner dirichlet --alpha $ALPHA --seed $SEED --weighting-scheme label --check-teachers-accuracy > output_distill_femnist_c${COHORTS}_s${SEED}_a${ALPHA}_p${PARTICIPATION}.log 2>&1
