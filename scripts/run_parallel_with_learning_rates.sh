465562509d40e861539eba7e853e901276d994bb
Martijn de Vos <martijn.devos@epfl.ch>
2024-08-02 13:11:42 +0200

    Added script to run different LRs

---
+++ scripts/run_parallel_with_learning_rates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -lt 2 ]; then
+  echo "Error: Please provide the command to run with a learning rate placeholder ({}) and a list of learning rates."
+  echo "Usage: $0 <command_to_run_with_lr_placeholder> <lr1> <lr1> ..."
+  exit 1
+fi
+
+command_to_run=$1
+shift 1  # Remove the first argument to keep only the seeds
+
+# Use the provided seeds and run the command with each seed in parallel
+printf "%s\n" "$@" | xargs -I {} -P "$#" bash -c "LEARNING_RATE={} && echo \"Running command with learning rate: \$LEARNING_RATE\" && ${command_to_run/\{\}/\$SEED} > output_\$LEARNING_RATE.log 2>&1"
