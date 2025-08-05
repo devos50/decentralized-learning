7008be9aab6336ebf76ab34e210251b1b505ebf5
Martijn de Vos <martijn.devos@epfl.ch>
2025-08-05 09:32:42 +0200

    Improved DL evaluation logic

--- simulations/dl/dl_simulation.py
+++ simulations/dl/dl_simulation.py
@@ -102,7 +102,8 @@ class DLSimulation(LearningSimulation):
         for node in self.nodes:
             node.overlays[0].start_round(self.round_nr)
         self.register_task("round_done", self.on_round_done, interval=self.args.dl_round_timeout)
-        self.register_task("check_accuracy", self.compute_all_accuracies, interval=self.args.accuracy_logging_interval)
+        if self.args.accuracy_logging_is_in_sec:
+            self.register_task("check_accuracy", self.compute_all_accuracies, interval=self.args.accuracy_logging_interval)
         await super().start_simulation()
 
     def on_round_done(self):
@@ -121,6 +122,10 @@ class DLSimulation(LearningSimulation):
 
         for node in self.nodes:
             node.overlays[0].aggregate_adapters()
+
+        # Should we check the accuracy?
+        if not self.args.accuracy_logging_is_in_sec and self.args.accuracy_logging_interval > 0 and self.round_nr % self.args.accuracy_logging_interval == 0:
+            self.compute_all_accuracies()
 
         if self.args.rounds and self.round_nr >= self.args.rounds:
             self.on_simulation_finished()
